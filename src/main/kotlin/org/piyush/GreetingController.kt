9acb85095370ef31c5a4ab2dd930aa700f673220
Piyush Chauhan <piyushchauhan2011@gmail.com>
2016-06-15 18:38:31 +1000

    Added route for customer and other JPA method from tutorial https://spring.io/guides/gs/accessing-data-jpa/

--- src/main/kotlin/org/piyush/GreetingController.kt
+++ src/main/kotlin/org/piyush/GreetingController.kt
@@ -18,11 +18,37 @@ class GreetingController {
     lateinit var repository: CustomerRepository
 
     @RequestMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Customer {
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        val greet = Greeting(counter.incrementAndGet(), "Hello, $name")
+        return greet
+    }
+
+    @RequestMapping("/customer")
+    fun customer(): Customer {
         val cust = Customer("Jack", "Bauer")
         repository.save(cust)
         log.info("Saved customer $cust")
-        val greet = Greeting(counter.incrementAndGet(), "Hello, $name")
+
+        repository.save(Customer("Chloe", "O'Brian"))
+        repository.save(Customer("Kim", "Bauer"))
+        repository.save(Customer("David", "Palmer"))
+        repository.save(Customer("Michelle", "Dessler"))
+
+        log.info("Customers found with findAll():")
+        log.info("-------------------------------")
+
+        repository.findAll().forEach { log.info(it.toString()) }
+        val customer = repository.findOne(1L)
+        log.info("Customer found with findOne(1L):")
+        log.info("--------------------------------")
+        log.info(customer.toString())
+        log.info("")
+
+        log.info("Customer found with findByLastName('Bauer'):")
+        log.info("--------------------------------------------")
+        repository.findByLastName("Bauer").forEach { log.info(it.toString()) }
+        log.info("")
+
         return cust
     }
 }
